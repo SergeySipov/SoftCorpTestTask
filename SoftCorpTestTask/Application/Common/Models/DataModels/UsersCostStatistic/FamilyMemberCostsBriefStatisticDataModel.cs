456e6379b295046e96a6c7e5f657e094a63dbfa8
Sergey Sipov <37745809+SergeySipov@users.noreply.github.com>
2023-07-31 20:47:27 +0300

    Fix controllers

---
+++ SoftCorpTestTask/Application/Common/Models/DataModels/UsersCostStatistic/FamilyMemberCostsBriefStatisticDataModel.cs
@@ -0,0 +1,9 @@
+﻿using Application.Common.Models.Common;
+
+namespace Application.Common.Models.DataModels.UsersCostStatistic;
+
+public record FamilyMemberCostsBriefStatisticDataModel
+{
+    public decimal TotalCosts { get; init; }
+    public PaginatedList<UsersCostsStatisticDataModel> FamilyMemberDetailedStatistic { get; init; }
+}
